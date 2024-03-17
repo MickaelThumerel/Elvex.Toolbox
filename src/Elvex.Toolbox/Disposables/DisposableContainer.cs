3158c5e5d3aec96eb38ebc0d42e425b65be8261a
mickael thumerel <mickael.thumerel@elvexoft.com>
2024-03-17 20:45:56 +0000

    Merged PR 83: Add disposable container & Move SupportInitialization
    - :art: (Dispoable) Add some disposable container
    - Fix build
    - Move to public some container
    - :bug: (Condition, Definition) Add missing DataMember attribute
    - :sparkles: (Support, Initialization) add implementation that can be used without inherit

---
+++ src/Elvex.Toolbox/Disposables/DisposableContainer.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Elvexoft.
+// The Elvexoft licenses this file to you under the MIT license.
+// Produce by Elvexoft & community
+
+namespace Elvex.Toolbox.Disposables
+{
+    public sealed class DisposableContainer<TContent> : SafeDisposable<TContent>
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisposableContainer{TContent}"/> class.
+        /// </summary>
+        public DisposableContainer(TContent content, bool disposeContent = false) 
+            : base(content, disposeContent)
+        {
+        }
+
+        #endregion
+    }
+}
