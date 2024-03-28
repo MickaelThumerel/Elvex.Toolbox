52b29ddf900ab9af78d3b979dca4cdb9e5afd4fc
mickael thumerel <mickael.thumerel@elvexoft.com>
2024-03-28 22:34:01 +0100

    :sparkles: (Span) Add replace many to replace multiple occurence by same value in one loop

---
+++ src/tests/Elvex.Toolbox.Core.UnitTests/Extensions/SpanExtensionsUTest.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Elvexoft.
+// The Elvexoft licenses this file to you under the MIT license.
+// Produce by Elvexoft & community
+
+namespace Elvex.Toolbox.Core.UnitTests.Extensions
+{
+    using NFluent;
+
+    using System;
+
+    /// <summary>
+    /// Test for <see cref="SpanExtensions"/>
+    /// </summary>
+    public sealed class SpanExtensionsUTest
+    {
+        [Theory]
+
+        // Simple
+        [InlineData("azertyuiopqsdfghjklmwxcvbn", '_', (char[])['a', 't', 'p'], "_zer_yuio_qsdfghjklmwxcvbn")]
+
+        // Multiple time same
+        [InlineData("azertyuiopqsdfghjklmwxcvbnazertyuiopqsdfghjklmwxcvbn", '_', (char[])['a', 't', 'p'], "_zer_yuio_qsdfghjklmwxcvbn_zer_yuio_qsdfghjklmwxcvbn")]
+
+        // Contigue & last 
+        [InlineData("azertyuiopqsdfghjklmwxcvbnazertyuiopqsdfghjklmwxcvbn", '_', (char[])['a', 't', 'p', 'n'], "_zer_yuio_qsdfghjklmwxcvb__zer_yuio_qsdfghjklmwxcvb_")]
+
+        // No replacement
+        [InlineData("azertyuiopqsdfghjklmwxcvbnazertyuiopqsdfghjklmwxcvbn", '_', (char[])[], "azertyuiopqsdfghjklmwxcvbnazertyuiopqsdfghjklmwxcvbn")]
+        public void Span_ReplaceMany(string source, char replacement, char[] search, string expected)
+        {
+            ReadOnlySpan<char> spanSource = source;
+
+            var result = spanSource.ReplaceMany(replacement, search);
+
+            Check.That(result.ToString()).IsNotEmpty().And.Equals(expected);
+        }
+    }
+}
