9500a817354a98231e8611d878c1510823d1a500
mickael thumerel <mickael.thumerel@elvexoft.com>
2024-03-28 23:14:14 +0100

    :sparkles: (Regex) Add MultiSpace regex

--- src/Elvex.Toolbox.Abstractions/Helpers/RegexHelper.cs
+++ src/Elvex.Toolbox.Abstractions/Helpers/RegexHelper.cs
@@ -4,11 +4,36 @@
 
 namespace Elvex.Toolbox.Abstractions.Helpers
 {
+    using System.Text.RegularExpressions;
+
     /// <summary>
     /// Helper Around regex
     /// </summary>
     public static class RegexHelper
     {
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:System.Object" /> class.
+        /// </summary>
+        static RegexHelper()
+        {
+            MultiSpace = new Regex(Pattern.MULTI_SPACE, RegexOptions.Compiled);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the multi space.
+        /// </summary>
+        public static Regex MultiSpace { get; }
+
+        #endregion
+
+        #region Nested
+
         /// <summary>
         /// Define regex classic pattern
         /// </summary>
@@ -18,6 +43,13 @@ namespace Elvex.Toolbox.Abstractions.Helpers
             /// The unique identifier pattern
             /// </summary>
             public const string GUID = "[a-zA-Z0-9]{8}[-]?[a-zA-Z0-9]{4}[-]?[a-zA-Z0-9]{4}[-]?[a-zA-Z0-9]{4}[-]?[a-zA-Z0-9]{12}";
+
+            /// <summary>
+            /// The multi space
+            /// </summary>
+            public const string MULTI_SPACE = "[\\s]+";
         }
+
+        #endregion
     }
 }
