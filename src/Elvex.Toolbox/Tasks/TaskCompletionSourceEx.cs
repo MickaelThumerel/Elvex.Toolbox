6decb56a0f78974da612a236b31e7bf561fff0d5
mickael thumerel <mickael.thumerel@elvexoft.com>
2024-04-05 17:38:30 +0200

    :sparkles: (TaskCompletion) Factorise class TaskCompletionSource under a same interface to simplify common behavior

---
+++ src/Elvex.Toolbox/Tasks/TaskCompletionSourceEx.cs
@@ -0,0 +1,128 @@
+﻿// Copyright (c) Elvexoft.
+// The Elvexoft licenses this file to you under the MIT license.
+// Produce by Elvexoft & community
+
+namespace Elvex.Toolbox.Tasks
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="TaskCompletionSource" />
+    /// <seealso cref="ITaskCompletionSourceEx" />
+    public sealed class TaskCompletionSourceEx : TaskCompletionSource, ITaskCompletionSourceEx
+    {
+        #region Properties
+
+        /// <inheritdoc />
+        public Type ExpectedResultType
+        {
+            get { return NoneType.Trait; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public Task GetTask()
+        {
+            return base.Task;
+        }
+
+        /// <inheritdoc />
+        public void SetResultObject<TGenericResult>(in TGenericResult? _ = default)
+        {
+            base.SetResult();
+        }
+
+        /// <inheritdoc />
+        public bool TrySetResultObject<TGenericResult>(in TGenericResult? _ = default)
+        {
+            return base.TrySetResult();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <seealso cref="TaskCompletionSource" />
+    /// <seealso cref="ITaskCompletionSourceEx" />
+    public sealed class TaskCompletionSourceEx<TResult> : TaskCompletionSource<TResult>, ITaskCompletionSourceEx
+    {
+        #region Fields
+
+        private static readonly Type s_resultTraits;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Initializes the <see cref="TaskCompletionSourceEx{TResult}"/> class.
+        /// </summary>
+        static TaskCompletionSourceEx()
+        {
+            s_resultTraits = typeof(TResult);
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <inheritdoc />
+        public Type ExpectedResultType
+        {
+            get { return s_resultTraits; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public Task GetTask()
+        {
+            return base.Task;
+        }
+
+        /// <inheritdoc />
+        public void SetResultObject<TGenericResult>(in TGenericResult? obj = default)
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            if (object.ReferenceEquals(obj, default))
+            {
+                base.SetResult(default);
+                return;
+            }
+            else if (obj is TResult result)
+            {
+                base.SetResult(result);
+                return;
+            }
+
+            throw new InvalidCastException("Generic result " + obj + " must be a " + typeof(TResult));
+#pragma warning restore CS8604 // Possible null reference argument.
+        }
+
+        /// <inheritdoc />
+        public bool TrySetResultObject<TGenericResult>(in TGenericResult? obj = default)
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            if (object.ReferenceEquals(obj, default))
+                return base.TrySetResult(default);
+            else if (obj is TResult result)
+                return base.TrySetResult(result);
+
+            throw new InvalidCastException("Generic result " + obj + " must be a " + typeof(TResult));
+#pragma warning restore CS8604 // Possible null reference argument.
+        }
+
+        #endregion
+    }
+}
---
+++ src/tests/Elvex.Toolbox.Core.UnitTests/Tasks/TaskCompletionSourceExUTests.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Elvexoft.
+// The Elvexoft licenses this file to you under the MIT license.
+// Produce by Elvexoft & community
+
+namespace Elvex.Toolbox.Core.UnitTests.Tasks
+{
+    using AutoFixture;
+
+    using Elvex.Toolbox.Tasks;
+    using Elvex.Toolbox.UnitTests.ToolKit.Helpers;
+
+    using NFluent;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Test for <see cref="ITaskCompletionSourceEx"/>
+    /// </summary>
+    public sealed class TaskCompletionSourceExUTests
+    {
+        #region Fields
+        
+        private readonly Fixture _fixture;
+        
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TaskCompletionSourceExUTests"/> class.
+        /// </summary>
+        public TaskCompletionSourceExUTests()
+        {
+            this._fixture = ObjectTestHelper.PrepareFixture();
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Theory]
+        [InlineData(typeof(NoneType),  typeof(TaskCompletionSourceEx))]
+        [InlineData(typeof(int), typeof(TaskCompletionSourceEx<int>))]
+        public void Generic_TaskCompletion(Type resultType, Type completionSourceType)
+        {
+            var result = (resultType == NoneType.Trait) ? NoneType.Instance : Activator.CreateInstance(resultType);
+
+            var completion = (ITaskCompletionSourceEx)Activator.CreateInstance(completionSourceType)!;
+
+            Check.That(completion).IsNotNull();
+            Check.That(completion.ExpectedResultType).IsEqualTo(resultType);
+
+            var task = completion.GetTask();
+
+            Check.That(task.Status).IsEqualTo(TaskStatus.WaitingForActivation);
+
+            completion.SetResultObject(result);
+
+            Check.That(task.Status).IsEqualTo(TaskStatus.RanToCompletion);
+
+            var taskResult = task.GetResult();
+
+            if (resultType == NoneType.Trait)
+                taskResult = NoneType.Instance;
+
+            Check.That(taskResult).IsNotNull().And.IsEqualTo(result);
+        }
+
+        [Theory]
+        [InlineData(typeof(TaskCompletionSourceEx))]
+        [InlineData(typeof(TaskCompletionSourceEx<int>))]
+        public void Generic_TaskCompletion_Cancellation(Type completionSourceType)
+        {
+            var completion = (ITaskCompletionSourceEx)Activator.CreateInstance(completionSourceType)!;
+
+            Check.That(completion).IsNotNull();
+
+            var task = completion.GetTask();
+            Check.That(task.Status).IsEqualTo(TaskStatus.WaitingForActivation);
+
+            completion.SetCanceled();
+            Check.That(task.Status).IsEqualTo(TaskStatus.Canceled);
+        }
+
+        [Theory]
+        [InlineData(typeof(TaskCompletionSourceEx))]
+        [InlineData(typeof(TaskCompletionSourceEx<int>))]
+        public void Generic_TaskCompletion_Exceptions(Type completionSourceType)
+        {
+            var completion = (ITaskCompletionSourceEx)Activator.CreateInstance(completionSourceType)!;
+
+            Check.That(completion).IsNotNull();
+
+            var task = completion.GetTask();
+            Check.That(task.Status).IsEqualTo(TaskStatus.WaitingForActivation);
+
+            var ex = new InvalidDataException();
+            completion.SetException(ex);
+            
+            Check.That(task.Status).IsEqualTo(TaskStatus.Faulted);
+
+            Check.That(task.Exception).IsNotNull();
+            Check.That(task.Exception!.InnerExceptions).IsNotNull().And.CountIs(1);
+            Check.That(task.Exception!.InnerExceptions.First()).IsNotNull().And.IsEqualTo(ex);
+        }
+
+        #endregion
+    }
+}
