d70ce3f83ab8f9929d519f7ebb230a9d0059b288
mickael thumerel <mickael.thumerel@elvexoft.com>
2024-06-20 08:14:15 +0000

    Intergration
    :sparkle: (ViewManager) Add simple Mono Content control view
    :sparkle: (Selector) add service to help service selector based on a dynamic key
    :sparkle: (Picker) add folder picker

---
+++ src/Elvex.Toolbox/Services/ISelectorItem.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Elvexoft.
+// The Elvexoft licenses this file to you under the MIT license.
+// Produce by Elvexoft & community
+
+namespace Elvex.Toolbox.Services
+{
+    /// <summary>
+    /// An item used in the selector <see cref="ISelectorService{TKey, TValue}"/>
+    /// </summary>
+    public interface ISelectorItem<TKey, TValue>
+        where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Gets the key.
+        /// </summary>
+        TKey Key { get; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        TValue Value { get; }
+    }
+}
---
+++ src/Elvex.Toolbox/Services/ISelectorService.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Elvexoft.
+// The Elvexoft licenses this file to you under the MIT license.
+// Produce by Elvexoft & community
+
+namespace Elvex.Toolbox.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISelectorService<TKey, TValue>
+        where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Gets the value based on configured data
+        /// </summary>
+        TValue? GetValue(in TKey key);
+    }
+}
